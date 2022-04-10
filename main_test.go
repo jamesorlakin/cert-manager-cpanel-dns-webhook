c909eef9794c362b236ad7cb7c4b122cfe3a6730
James Lakin <james@jameslakin.co.uk>
2022-04-10 13:45:21 +0100

    Temporarily comment out test

--- main_test.go
+++ main_test.go
@@ -1,40 +1,29 @@
 package main
 
 import (
-	"os"
 	"testing"
-
-	"github.com/jamesorlakin/cert-manager-cpanel-dns-webhook/example"
-	"github.com/jetstack/cert-manager/test/acme/dns"
+	// "github.com/jetstack/cert-manager/test/acme/dns"
 )
 
 var (
-	zone = os.Getenv("TEST_ZONE_NAME")
+	zone = "test-domain.com."
 )
 
 func TestRunsSuite(t *testing.T) {
+	// TODO(jamesorlakin): Need to test main webhook logic. Not sure how to fake a k8s client secret?
+
 	// The manifest path should contain a file named config.json that is a
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
-	//
 
-	// Uncomment the below fixture when implementing your custom DNS provider
-	//fixture := dns.NewFixture(&customDNSProviderSolver{},
-	//	dns.SetResolvedZone(zone),
-	//	dns.SetAllowAmbientCredentials(false),
-	//	dns.SetManifestPath("testdata/my-custom-solver"),
-	//	dns.SetBinariesPath("_test/kubebuilder/bin"),
-	//)
-	solver := example.New("59351")
-	fixture := dns.NewFixture(solver,
-		dns.SetResolvedZone("example.com."),
-		dns.SetManifestPath("testdata/my-custom-solver"),
-		dns.SetDNSServer("127.0.0.1:59351"),
-		dns.SetUseAuthoritative(false),
-	)
+	// fixture := dns.NewFixture(&customDNSProviderSolver{},
+	// 	dns.SetResolvedZone(zone),
+	// 	dns.SetAllowAmbientCredentials(false),
+	// 	dns.SetManifestPath("testdata/my-custom-solver"),
+	// )
 	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
 	//fixture.RunConformance(t)
-	fixture.RunBasic(t)
-	fixture.RunExtended(t)
+	// fixture.RunBasic(t)
+	// fixture.RunExtended(t)
 
 }
